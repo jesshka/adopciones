ef76e20e2e4e15e72a850b9c0427e3622ae5e618
Jessica <jessibrandy7@gmail.com>
2020-06-12 22:55:29 -0500

    Add basic routing to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,76 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { AdoptionsComponent } from './main-views/adoptions/adoptions.component';
+import { AdoptionDetailComponent } from './main-views/adoptions/adoption-detail/adoption-detail.component';
+import { AdoptionFormComponent } from './main-views/adoptions/adoption-form/adoption-form.component';
+import { BreedDetailComponent } from './main-views/breed-list/breed-detail/breed-detail.component';
+import { BreedListComponent } from './main-views/breed-list/breed-list.component';
+import { ContactUsComponent } from './main-views/contact-us/contact-us.component';
+import { HomeComponent } from './main-views/home/home.component';
+import { MainLayoutComponent } from './main-views/main-layout/main-layout.component';
+import { Page404Component } from './main-views/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: '',
+            component: HomeComponent,
+          },
+          {
+            path: '**',
+            redirectTo: '/404',
+          },
+        ],
+      },
+      {
+        path: '404',
+        component: Page404Component,
+      },
+      {
+        path: 'adopciones',
+        children: [
+          {
+            path: '',
+            component: AdoptionsComponent,
+          },
+          {
+            path: ':id',
+            component: AdoptionDetailComponent,
+          },
+          {
+            path: 'formulario/:id',
+            component: AdoptionFormComponent,
+          },
+        ],
+      },
+      {
+        path: 'razas',
+        children: [
+          {
+            path: '',
+            component: BreedListComponent,
+          },
+          {
+            path: ':raza',
+            component: BreedDetailComponent,
+          }
+        ],
+      },
+      {
+        path: 'contactanos',
+        component: ContactUsComponent,
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main-views/adoptions/adoption-detail/adoption-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adoption-detail',
+  templateUrl: './adoption-detail.component.html',
+  styleUrls: ['./adoption-detail.component.scss']
+})
+export class AdoptionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
